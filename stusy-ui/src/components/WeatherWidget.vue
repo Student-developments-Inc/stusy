5550936b36e6ef34924c552359d5de8887f32910
Max <ya.max1999-2010@yandex.ru>
2022-07-07 13:54:47 +0300

    refactor: weather icons replaced to emoji

--- stusy-ui/src/components/WeatherWidget.vue
+++ stusy-ui/src/components/WeatherWidget.vue
@@ -1,10 +1,13 @@
 <template>
   <div class="block" id="weather">
-    <img
-        id="wicon"
-        v-bind:src="require(`../assets/weather/${weatherCode}.svg`)"
-        alt="weathericon"
-    />
+    <!--    <img-->
+    <!--        id="wicon"-->
+    <!--        v-bind:src="require(`../assets/weather/${weatherCode}.svg`)"-->
+    <!--        alt="weathericon"-->
+    <!--      />-->
+    <span id="wicon">
+      {{ weatherSymbols[weatherCodes[weatherCode]] }}
+    </span>
     <p id="dateNowDay">Сегодня {{ getNowDay() }}, сейчас {{ weather }}&deg;</p>
     <p>У вас 4 пары</p>
     <p>Температура к концу занятий {{ getDiff() + lastLessonWeather }}°</p>
@@ -21,7 +24,79 @@ export default {
       weather: "",
       lastLessonTime: 1705,
       lastLessonWeather: "",
-      weatherCode: 185
+      weatherCode: 0,
+      weatherCodes: {
+        "0": "Unknown",
+        "113": "Sunny",
+        "116": "PartlyCloudy",
+        "119": "Cloudy",
+        "122": "VeryCloudy",
+        "143": "Fog",
+        "176": "LightShowers",
+        "179": "LightSleetShowers",
+        "182": "LightSleet",
+        "185": "LightSleet",
+        "200": "ThunderyShowers",
+        "227": "LightSnow",
+        "230": "HeavySnow",
+        "248": "Fog",
+        "260": "Fog",
+        "263": "LightShowers",
+        "266": "LightRain",
+        "281": "LightSleet",
+        "284": "LightSleet",
+        "293": "LightRain",
+        "296": "LightRain",
+        "299": "HeavyShowers",
+        "302": "HeavyRain",
+        "305": "HeavyShowers",
+        "308": "HeavyRain",
+        "311": "LightSleet",
+        "314": "LightSleet",
+        "317": "LightSleet",
+        "320": "LightSnow",
+        "323": "LightSnowShowers",
+        "326": "LightSnowShowers",
+        "329": "HeavySnow",
+        "332": "HeavySnow",
+        "335": "HeavySnowShowers",
+        "338": "HeavySnow",
+        "350": "LightSleet",
+        "353": "LightShowers",
+        "356": "HeavyShowers",
+        "359": "HeavyRain",
+        "362": "LightSleetShowers",
+        "365": "LightSleetShowers",
+        "368": "LightSnowShowers",
+        "371": "HeavySnowShowers",
+        "374": "LightSleetShowers",
+        "377": "LightSleet",
+        "386": "ThunderyShowers",
+        "389": "ThunderyHeavyRain",
+        "392": "ThunderySnowShowers",
+        "395": "HeavySnowShowers",
+      },
+      weatherSymbols: {
+        "Unknown": "✨",
+        "Cloudy": "☁️",
+        "Fog": "🌫",
+        "HeavyRain": "🌧",
+        "HeavyShowers": "🌧",
+        "HeavySnow": "❄️",
+        "HeavySnowShowers": "❄️",
+        "LightRain": "🌦",
+        "LightShowers": "🌦",
+        "LightSleet": "🌧",
+        "LightSleetShowers": "🌧",
+        "LightSnow": "🌨",
+        "LightSnowShowers": "🌨",
+        "PartlyCloudy": "⛅️",
+        "Sunny": "☀️",
+        "ThunderyHeavyRain": "🌩",
+        "ThunderyShowers": "⛈",
+        "ThunderySnowShowers": "⛈",
+        "VeryCloudy": "☁️",
+      }
     };
   },
   methods: {
@@ -83,7 +158,7 @@ export default {
 #weather #wicon {
   position: absolute;
   right: -10px;
-  top: -10px;
-  height: 100px;
+  top: -15px;
+  font-size: 85pt;
 }
 </style>
