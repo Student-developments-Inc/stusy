856bf1d6763580e353346d4e5078eb510894f76f
Max <ya.max1999-2010@yandex.ru>
2022-07-12 15:58:24 +0300

    refactor: href changed

--- stusy-ui/src/components/AsideMenu.vue
+++ stusy-ui/src/components/AsideMenu.vue
@@ -7,7 +7,7 @@ refactor:<template>
           <img src="@/assets/home.svg">
         </div>
         <span style="color: var(--light)">Домашняя страница</span></a></li>
-      <li><a href="/courses">
+      <li><a href="/mycourses">
         <div class="LeftMenu_icon">
           <img src="@/assets/book.svg">
         </div>
