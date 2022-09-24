e99cd0b7ab8856eced98b5c816bc6c3e1437ffed
Max <ya.max1999-2010@yandex.ru>
2022-09-24 13:23:20 +0300

    feat: Добавлены страницы слоёв

---
+++ stusy-ui/src/layouts/EmptyLayout.vue
@@ -0,0 +1,14 @@
+<template>
+  <p>Пустой слой</p>
+  <router-view/>
+</template>
+
+<script>
+export default {
+  name: "EmptyLayout"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ stusy-ui/src/layouts/HomeLayout.vue
@@ -0,0 +1,24 @@
+<template>
+  <p>Домашний слой</p>
+  <div class="homepage">
+    <AsideMenu/>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+import AsideMenu from "@/components/AsideMenu";
+
+export default {
+  name: "HomeLayout",
+  components: {
+    AsideMenu
+  }
+}
+</script>
+
+<style scoped>
+.homepage {
+  display: flex !important;
+}
+</style>
