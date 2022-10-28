f600d08a3abd5cabb5b1f759a99da098e0c9536c
Max <ya.max1999-2010@yandex.ru>
2022-10-28 21:58:17 +0300

    feat: Виджет с расписанием выделен в отдельный компонент

--- stusy-ui/src/views/HomeView.vue
+++ stusy-ui/src/views/HomeView.vue
@@ -1,65 +1,7 @@
 <template>
   <div class="mainContent">
     <WeatherWidget/>
-    <div class="block" id="schedule">
-      <h1>{{ localeDate }}</h1>
-      <div class="block-schedule">
-        <div>
-          <ul class="block-schedule_topLeft-content">
-            <li>8:40–10:15</li>
-            <li>лекц.</li>
-            <li>3-612</li>
-          </ul>
-          <ul class="block-schedule_topRight-content">
-            <li class="right-post">доцент</li>
-            <li>Смирин О.А.</li>
-          </ul>
-        </div>
-        <p>Теория вероятностей. Математическая статистика.</p>
-      </div>
-      <div class="block-schedule">
-        <div>
-          <ul class="block-schedule_topLeft-content">
-            <li>8:40–10:15</li>
-            <li>лекц.</li>
-            <li>3-612</li>
-          </ul>
-          <ul class="block-schedule_topRight-content">
-            <li class="right-post">доцент</li>
-            <li>Смирин О.А.</li>
-          </ul>
-        </div>
-        <p>Теория вероятностей. Математическая статистика.</p>
-      </div>
-      <div class="block-schedule">
-        <div>
-          <ul class="block-schedule_topLeft-content">
-            <li>8:40–10:15</li>
-            <li>лекц.</li>
-            <li>3-612</li>
-          </ul>
-          <ul class="block-schedule_topRight-content">
-            <li class="right-post">доцент</li>
-            <li>Смирин О.А.</li>
-          </ul>
-        </div>
-        <p>Теория вероятностей. Математическая статистика.</p>
-      </div>
-      <div class="block-schedule">
-        <div>
-          <ul class="block-schedule_topLeft-content">
-            <li>8:40–10:15</li>
-            <li>лекц.</li>
-            <li>3-612</li>
-          </ul>
-          <ul class="block-schedule_topRight-content">
-            <li class="right-post">доцент</li>
-            <li>Смирин О.А.</li>
-          </ul>
-        </div>
-        <p>Теория вероятностей. Математическая статистика.</p>
-      </div>
-    </div>
+    <TimetableWidget/>
     <div class="block" id="courses">
       <div>
         <h1>Ваши курсы</h1>
@@ -81,28 +23,21 @@
 <script>
 import {getCookie} from "@/global";
 import WeatherWidget from "@/components/WeatherWidget";
+import TimetableWidget from "@/components/TimetableWidget"
 
 export default {
   name: "HomeView",
-  components: {WeatherWidget},
+  components: {WeatherWidget, TimetableWidget},
   data() {
     return {
-      localeDate: ""
     };
   },
   methods: {
-    getDate() {
-      const options = {weekday: 'long', month: 'numeric', day: 'numeric'};
-      this.localeDate = new Date().toLocaleDateString(undefined, options)
-      this.localeDate = this.localeDate.charAt(0).toUpperCase() + this.localeDate.slice(1)
-    },
-    menuAction() {
+      menuAction() {
       this.menu = !this.menu;
     }
   },
   mounted() {
-    this.getDate()
-
     if (!getCookie("TOKEN")) {
       this.$router.push("/auth");
     }
@@ -167,101 +102,9 @@ export default {
   text-align: right;
 }
 
-#schedule {
-  display: flex;
-
-  flex-direction: column;
-  padding: 18px 23px;
-}
-
-#schedule h1 {
-  margin: 0;
-  font-style: normal;
-  font-weight: 500;
-  font-size: 24px;
-  color: var(--dark);
-}
-
-.block-schedule {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  padding: 7px 10px;
-  margin-top: 15px;
-  margin-bottom: -5px;
-  box-shadow: 0 0 4px rgba(0, 0, 0, 0.25);
-  border-radius: 15px;
-}
-
-.block-schedule div {
-  display: inline-block;
-  width: 100%;
-  flex: 1 1 auto;
-}
-
-.block-schedule_topLeft-content, .block-schedule_topRight-content {
-  display: flex;
-  align-items: center;
-  margin: 0;
-  float: left;
-  padding: 0;
-  list-style: none;
-}
-
-.block-schedule_topLeft-content {
-  float: left;
-}
-
-.block-schedule_topLeft-content li {
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  text-align: center;
-  padding: 4px 20px;
-  font-style: normal;
-  font-weight: 400;
-  font-size: 14px;
-  line-height: 16px;
-  margin: 0 8px 0 0;
-  border: 1px solid var(--dark);
-  border-radius: 25px;
-}
-
-.block-schedule_topRight-content {
-  float: right;
-}
-
-.right-post {
-  text-align: left;
-  margin: 0 -20px 0 0;
-  padding: 4px 35px 4px 20px;
-}
-
-.block-schedule_topRight-content li {
-  flex: 1 1 auto;
-  margin: 0 0 0 4px;
-  text-align: center;
-  padding: 4px 20px;
-  font-style: normal;
-  font-weight: 400;
-  font-size: 14px;
-  line-height: 16px;
-  box-shadow: 0 0 4px rgba(0, 0, 0, 0.25);
-  border-radius: 25px;
-  background: var(--light);
-}
-
-.block-schedule p {
-  margin: 15px 0;
-}
-
 @media (max-width: 1600px) {
   .block {
     width: 100%;
-  }
-
-  .block-schedule div {
-    width: auto;
   }
 }
 </style>
