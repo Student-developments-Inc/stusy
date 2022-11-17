82febde666f540696d6c3b7a6bc5127461d9ef85
Max <ya.max1999-2010@yandex.ru>
2022-11-17 11:34:11 +0300

    fix: Удалены неиспользуемые переменные

--- stusy-ui/src/layouts/HomeLayout.vue
+++ stusy-ui/src/layouts/HomeLayout.vue
@@ -15,52 +15,6 @@
 <script setup>
 import AsideMenu from "@/components/AsideMenu";
 import TopMenu from "@/components/TopMenu";
-import {getCookie, logout, url} from "@/global";
-import {computed, onMounted, ref} from "vue";
-
-//userData передать в TopMenu
-
-onMounted(() => {
-  getUserData();
-})
-
-const userData = ref(null)
-
-const localeHours = computed(() => {
-  let localeHours = new Date().getHours();
-  if (localeHours > 3 && localeHours < 12) return "Доброе утро";
-  else if (localeHours > 11 && localeHours < 19) return "Добрый день";
-  else if (localeHours > 18 && localeHours < 24) return "Добрый вечер";
-  else if (localeHours > 23 || localeHours < 4) return "Привет";
-  throw Error('localHours error')
-})
-
-const name = computed(() => {
-  if (userData.value === null) {
-    return '';
-  }
-  return `, ${userData.value.first_name}`;
-})
-
-function getUserData() {
-  if (getCookie("ID") === undefined) logout();
-  fetch(`${url}/users/${getCookie("ID")}`, {
-    headers: {
-      "Authorization": `Bearer ${getCookie("TOKEN")}`
-    }
-  }).then(response => {
-    if (response.ok) return response.json();
-    if (response.status === 401) {
-      logout();
-    }
-  }).then(data => {
-    if (data !== undefined) {
-      userData.value = data;
-    }
-  }).catch(err => {
-    console.error("Cannot fetch", err);
-  });
-}
 
 </script>
 
