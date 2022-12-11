7f27e19d419fe8c7b6cbf230d38dfe4714ab876e
Max <ya.max1999-2010@yandex.ru>
2022-12-11 20:45:38 +0300

    refactor: В поля вставлены данные из пользовательских данных

--- stusy-ui/src/views/ProfileView.vue
+++ stusy-ui/src/views/ProfileView.vue
@@ -6,15 +6,16 @@
           <img src="@/assets/avatar.jpg" alt="Фото профиля" draggable="false">
         </div>
         <div class="nameNum">
-          <p class="name">Валентин Павлов</p>
-          <p>Номер студенческого: 12658496</p>
+          <p class="name">{{ userData.first_name }} {{ userData.last_name }}</p>
+          <p>Номер студенческого: {{ userData.id }}</p>
         </div>
       </div>
-      <form action="#">
+      <form v-on:submit.prevent="submit(userData)">
         <div class="personalDataForm">
           <label class="input-form">
             <p>Имя</p>
             <input
+                v-model="userData.first_name"
                 type="text"
                 placeholder="Введите имя"
                 required
@@ -23,6 +24,7 @@
           <label class="input-form">
             <p>Фамилия</p>
             <input
+                v-model="userData.last_name"
                 type="text"
                 placeholder="Введите фамилию"
                 required
@@ -42,135 +44,162 @@
       </form>
 
     </div>
-    <!--    <div class="AvatarAndPersonalDataAndContacts">-->
-    <!--      <div class="AvatarProfile">-->
-    <!--        <div id="AvatarProfileMedia">АВАТАРКИ НЕТУ</div>-->
-    <!--        <div class="button-login">-->
-    <!--          <input type="submit" value="Загрузить новое фото"/>-->
-    <!--        </div>-->
-    <!--      </div>-->
-    <!--      <div class="personalData">-->
-    <!--        <h1>Личные данные</h1>-->
-    <!--        <div class="personalDataContent">-->
-    <!--          <form class="personalDataContentForm">-->
-    <!--            <label class="input-form">-->
-    <!--              <h2>Имя</h2>-->
-    <!--              <input-->
-    <!--                  type="text"-->
-    <!--                  placeholder="Введите имя"-->
-    <!--                  required-->
-    <!--              />-->
-    <!--            </label>-->
-    <!--            <label class="input-form">-->
-    <!--              <h2>Фамилия</h2>-->
-    <!--              <input-->
-    <!--                  type="text"-->
-    <!--                  placeholder="Введите фамилию"-->
-    <!--                  required-->
-    <!--              />-->
-    <!--            </label>-->
-    <!--            <label class="input-form">-->
-    <!--              <h2>Отчество</h2>-->
-    <!--              <input-->
-    <!--                  type="text"-->
-    <!--                  placeholder="Введите отчество"-->
-    <!--              />-->
-    <!--            </label>-->
+    <!--        <div class="AvatarAndPersonalDataAndContacts">-->
+    <!--          <div class="AvatarProfile">-->
+    <!--            <div id="AvatarProfileMedia">АВАТАРКИ НЕТУ</div>-->
     <!--            <div class="button-login">-->
-    <!--              <input type="submit" value="Сохранить"/>-->
+    <!--              <input type="submit" value="Загрузить новое фото"/>-->
     <!--            </div>-->
-    <!--          </form>-->
-    <!--          <form class="personalDataContentForm">-->
+    <!--          </div>-->
+    <!--          <div class="personalData">-->
+    <!--            <h1>Личные данные</h1>-->
+    <!--            <div class="personalDataContent">-->
+    <!--              <form class="personalDataContentForm">-->
+    <!--                <label class="input-form">-->
+    <!--                  <h2>Имя</h2>-->
+    <!--                  <input-->
+    <!--                      type="text"-->
+    <!--                      placeholder="Введите имя"-->
+    <!--                      required-->
+    <!--                  />-->
+    <!--                </label>-->
+    <!--                <label class="input-form">-->
+    <!--                  <h2>Фамилия</h2>-->
+    <!--                  <input-->
+    <!--                      type="text"-->
+    <!--                      placeholder="Введите фамилию"-->
+    <!--                      required-->
+    <!--                  />-->
+    <!--                </label>-->
+    <!--                <label class="input-form">-->
+    <!--                  <h2>Отчество</h2>-->
+    <!--                  <input-->
+    <!--                      type="text"-->
+    <!--                      placeholder="Введите отчество"-->
+    <!--                  />-->
+    <!--                </label>-->
+    <!--                <div class="button-login">-->
+    <!--                  <input type="submit" value="Сохранить"/>-->
+    <!--                </div>-->
+    <!--              </form>-->
+    <!--              <form class="personalDataContentForm">-->
+    <!--                <label class="input-form">-->
+    <!--                  <h2>Дата рождения</h2>-->
+    <!--                  <input-->
+    <!--                      type="text"-->
+    <!--                      placeholder="хх.хх.хххх"-->
+    <!--                      required-->
+    <!--                  />-->
+    <!--                </label>-->
+    <!--                <label class="input-form">-->
+    <!--                  <h2>Пол</h2>-->
+    <!--                  <label class="radio-button">-->
+    <!--                    <input-->
+    <!--                        name="gender"-->
+    <!--                        type="radio"-->
+    <!--                        value="male"-->
+    <!--                    />-->
+    <!--                    <span>Мужской</span>-->
+    <!--                  </label>-->
+    <!--                  <label class="radio-button">-->
+    <!--                    <input-->
+    <!--                        name="gender"-->
+    <!--                        type="radio"-->
+    <!--                        value="female"-->
+    <!--                    />-->
+    <!--                    <span>Женский</span>-->
+    <!--                  </label>-->
+    <!--                </label>-->
+    <!--              </form>-->
+    <!--            </div>-->
+    <!--          </div>-->
+    <!--          <div class="contactsData">-->
+    <!--            <h1>Контакты</h1>-->
+    <!--            <form class="contactsDataForm">-->
+    <!--              <label class="input-form">-->
+    <!--                <h2>Номер телефона</h2>-->
+    <!--                <input-->
+    <!--                    type="text"-->
+    <!--                    placeholder="Введите номер телефона"-->
+    <!--                />-->
+    <!--              </label>-->
+    <!--              <label class="input-form">-->
+    <!--                <h2>Адрес электронной почты</h2>-->
+    <!--                <input-->
+    <!--                    type="text"-->
+    <!--                    placeholder="name@gmail.com"-->
+    <!--                    required-->
+    <!--                />-->
+    <!--              </label>-->
+    <!--            </form>-->
+    <!--          </div>-->
+    <!--        </div>-->
+    <!--        <div class="aboutMe">-->
+    <!--          <h1>Обо мне</h1>-->
+    <!--          <textarea class="aboutMeInput" placeholder="Расскажите о себе вкратце"></textarea>-->
+    <!--        </div>-->
+    <!--        <div class="linksToSocialNetworks">-->
+    <!--          <h1>Ссылки на соц. сети</h1>-->
+    <!--          <form class="linksToSocialNetworksForm">-->
     <!--            <label class="input-form">-->
-    <!--              <h2>Дата рождения</h2>-->
+    <!--              <h2>Vk.com</h2>-->
     <!--              <input-->
     <!--                  type="text"-->
-    <!--                  placeholder="хх.хх.хххх"-->
-    <!--                  required-->
+    <!--                  placeholder="vk.com"-->
     <!--              />-->
     <!--            </label>-->
     <!--            <label class="input-form">-->
-    <!--              <h2>Пол</h2>-->
-    <!--              <label class="radio-button">-->
-    <!--                <input-->
-    <!--                    name="gender"-->
-    <!--                    type="radio"-->
-    <!--                    value="male"-->
-    <!--                />-->
-    <!--                <span>Мужской</span>-->
-    <!--              </label>-->
-    <!--              <label class="radio-button">-->
-    <!--                <input-->
-    <!--                    name="gender"-->
-    <!--                    type="radio"-->
-    <!--                    value="female"-->
-    <!--                />-->
-    <!--                <span>Женский</span>-->
-    <!--              </label>-->
+    <!--              <h2>Facebook</h2>-->
+    <!--              <input-->
+    <!--                  type="text"-->
+    <!--                  placeholder="facebook.com"-->
+    <!--              />-->
+    <!--            </label>-->
+    <!--            <label class="input-form">-->
+    <!--              <h2>Instagram</h2>-->
+    <!--              <input-->
+    <!--                  type="text"-->
+    <!--                  placeholder="instagram.com"-->
+    <!--              />-->
     <!--            </label>-->
     <!--          </form>-->
     <!--        </div>-->
-    <!--      </div>-->
-    <!--      <div class="contactsData">-->
-    <!--        <h1>Контакты</h1>-->
-    <!--        <form class="contactsDataForm">-->
-    <!--          <label class="input-form">-->
-    <!--            <h2>Номер телефона</h2>-->
-    <!--            <input-->
-    <!--                type="text"-->
-    <!--                placeholder="Введите номер телефона"-->
-    <!--            />-->
-    <!--          </label>-->
-    <!--          <label class="input-form">-->
-    <!--            <h2>Адрес электронной почты</h2>-->
-    <!--            <input-->
-    <!--                type="text"-->
-    <!--                placeholder="name@gmail.com"-->
-    <!--                required-->
-    <!--            />-->
-    <!--          </label>-->
-    <!--        </form>-->
-    <!--      </div>-->
-    <!--    </div>-->
-    <!--    <div class="aboutMe">-->
-    <!--      <h1>Обо мне</h1>-->
-    <!--      <textarea class="aboutMeInput" placeholder="Расскажите о себе вкратце"></textarea>-->
-    <!--    </div>-->
-    <!--    <div class="linksToSocialNetworks">-->
-    <!--      <h1>Ссылки на соц. сети</h1>-->
-    <!--      <form class="linksToSocialNetworksForm">-->
-    <!--        <label class="input-form">-->
-    <!--          <h2>Vk.com</h2>-->
-    <!--          <input-->
-    <!--              type="text"-->
-    <!--              placeholder="vk.com"-->
-    <!--          />-->
-    <!--        </label>-->
-    <!--        <label class="input-form">-->
-    <!--          <h2>Facebook</h2>-->
-    <!--          <input-->
-    <!--              type="text"-->
-    <!--              placeholder="facebook.com"-->
-    <!--          />-->
-    <!--        </label>-->
-    <!--        <label class="input-form">-->
-    <!--          <h2>Instagram</h2>-->
-    <!--          <input-->
-    <!--              type="text"-->
-    <!--              placeholder="instagram.com"-->
-    <!--          />-->
-    <!--        </label>-->
-    <!--      </form>-->
-    <!--    </div>-->
   </div>
 </template>
 
-<script>
+<script setup>
+import {getUserData} from "@/composables/getUserData";
+import {getCookie, url} from "@/global";
 
-export default {
-  name: "ProfileView",
-  components: {},
-};
+const {userData} = getUserData();
+
+function submit(userData) {
+  fetch(`${url}/users/${getCookie("ID")}`, {
+    method: "PUT",
+    headers: {
+      "Authorization": `Bearer ${getCookie("TOKEN")}`
+    },
+    body: JSON.stringify({
+      "first_name": userData.first_name,
+      "last_name": userData.last_name
+    })
+  }).then(response => {
+    if (response.ok) return response.json();
+    switch (response.status) {
+      case 400:
+        console.log("Неверные данные");
+        break;
+      case 403:
+        console.log("Нет доступа")
+        break;
+    }
+  })
+      // .then(data => {
+    // console.log(data);
+  // }).catch(err => {
+  //   console.error("Cannot fetch" + err);
+  // });
+}
 </script>
 
 <style scoped>
