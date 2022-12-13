5f861f039b17504acde022627f1c08665c9fc168
andem <naqrz@pm.me>
2022-12-13 15:50:41 +0300

    refactor(api.go): project structure changes
    changed some endpoints, do GET at / to see new endpoints

--- stusy-api/api.go
+++ stusy-api/api.go
@@ -3,7 +3,6 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
@@ -12,32 +11,120 @@ import (
 	"strings"
 	"time"
 
-	"github.com/dgrijalva/jwt-go"
+	"github.com/golang-jwt/jwt/v4"
 	"github.com/gorilla/mux"
 	"golang.org/x/crypto/bcrypt"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
+// DB Models
 type User struct {
-	ID        uint
-	Email     string
-	Password  []byte
-	Token     string
-	ExpiresAt int64
+	ID        uint		`json:"id"`
+	Email     string	`json:"email"`
+	Password  []byte	`json:"password"`
+	Token     string	`json:"token"`
+	ExpiresAt int64		`json:"expires_at"`
 }
 
 type UserData struct {
-	ID        uint
-	UserID    uint
-	FirstName string
-	LastName  string
+	ID		uint
+	UserID		uint	`json:"id"`
+	FirstName	string	`json:"first_name"`
+	MiddleName	string	`json:"middle_name"`
+	LastName	string	`json:"last_name"`
 }
 
-type Error struct {
-	Message string `json:"error_message"`
+// Command structs
+type Api struct {
+	Config
+	Router	*mux.Router
+	DB	*gorm.DB
 }
 
+type Config struct {
+	Port	string
+	DSN	string
+	Secret	string
+}
+
+// Main command functions' implementations
+func (a *Api) Init() {
+	var err error
+
+	log.Println("Initializing config struct..")
+	err = a.Config.Init()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for {
+		a.DB, err = gorm.Open(mysql.Open(a.Config.DSN), &gorm.Config{})
+		if err != nil {
+			log.Println("Failed to establish database connection, retrying...")
+			time.Sleep(time.Second * 3)
+		} else {
+			log.Println("Database connection established.")
+			break
+		}
+	}
+
+	err = a.DB.AutoMigrate(&User{}, &UserData{})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Println("Initializing routes..")
+	a.Router = mux.NewRouter()
+	a.initRoutes()
+}
+
+func (c *Config) Init() error {
+	c.DSN = fmt.Sprintf("%s:%s@tcp(stusy-db)/%s", os.Getenv("DB_USER"),
+				os.Getenv("DB_PASS"), os.Getenv("DB_NAME"))
+
+	c.Port = os.Getenv("PORT")
+
+	c.Secret = os.Getenv("SECRET")
+
+	if len(c.Port) == 0 || len(c.Secret) == 0 {
+		return fmt.Errorf("Config init failed. Some of the environment variables are blank.")
+	}
+
+	return nil
+}
+
+func (a *Api) initRoutes() {
+	a.Router.HandleFunc("/", a.listRoutes).Methods("GET")
+
+	a.Router.HandleFunc("/users", a.getUsers).Methods("GET")
+	a.Router.HandleFunc("/user", a.createUser).Methods("POST")
+	a.Router.HandleFunc("/user/auth", a.loginUser).Methods("POST")
+	a.Router.HandleFunc("/user/{id:[0-9]+}", a.isAuth(a.checkPerms(a.getUser))).Methods("GET")
+	a.Router.HandleFunc("/user/{id:[0-9]+}", a.isAuth(a.checkPerms(a.updateUser))).Methods("PUT")
+	a.Router.HandleFunc("/user/{id:[0-9]+}", a.deleteUser).Methods("DELETE")
+
+	a.Router.HandleFunc("/courses", a.isAuth(a.getCourses)).Methods("GET")
+	a.Router.HandleFunc("/course", a.isAuth(a.createCourse)).Methods("POST")
+	a.Router.HandleFunc("/course/{id:[0-9]+}", a.isAuth(a.getCourse)).Methods("GET")
+	a.Router.HandleFunc("/course/{id:[0-9]+}", a.isAuth(a.updateCourse)).Methods("PUT")
+	a.Router.HandleFunc("/course/{id:[0-9]+}", a.isAuth(a.deleteCourse)).Methods("DELETE")
+}
+
+func (a *Api) Run(port string) {
+	log.Fatal(http.ListenAndServe(port, a.Router))
+}
+
+// Driver
+func main() {
+	a := Api{}
+	a.Init()
+
+	log.Println("Starting local server..")
+	a.Run(":" + a.Config.Port)
+}
+
+// Utilities
 const (
 	ErrorInternal       = "Internal server error. Try again later."
 	ErrorBadJSON        = "You have supplied an invalid JSON body."
@@ -51,414 +138,310 @@ const (
 	ErrorNotFound       = "Resource not found."
 )
 
-var (
-	secret	string
-	port	string
-	dsn	string
-	db	*gorm.DB
-	r	*mux.Router
-)
+func RespondWithJSON(w *http.ResponseWriter, status int, payload interface{}) {
+	res, _ := json.MarshalIndent(payload, "", "	")
 
-// Init funcs
-func
-initdb() error {
-	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
-	if err != nil {
-		return err
-	}
-
-	err = db.AutoMigrate(&User{}, &UserData{})
-	if err != nil {
-		return err
-	}
-
-	return nil
-}
-
-func
-initcfg() error {
-	dsn = fmt.Sprintf("%s:%s@tcp(stusy-db)/%s", os.Getenv("DB_USER"), os.Getenv("DB_PASS"), os.Getenv("DB_NAME"))
-	port = ":" + os.Getenv("PORT")
-	secret = os.Getenv("SECRET")
-
-	if port == "" || secret == "" {
-		return fmt.Errorf("some of the environment variables are blank")
-	}
-
-	return nil
-}
-
-func
-initrouter() *mux.Router {
-	r = mux.NewRouter()
-
-	r.HandleFunc("/", listroutes()).Methods("GET", "OPTIONS")
-//	r.HandleFunc("/users", Count()).Methods("GET", "OPTIONS")
-	r.HandleFunc("/users/login", userlogin()).Methods("POST", "OPTIONS")
-	r.HandleFunc("/users/register", usercreation()).Methods("POST", "OPTIONS")
-	r.HandleFunc("/users/{id:[0-9]+}", auth(userinfo())).Methods("GET", "PUT", "OPTIONS")
-
-	r.Use(mux.CORSMethodMiddleware(r))
-
-	return r
-}
-
-// Handler funcs
-func
-listroutes() http.HandlerFunc {
-	return func(w http.ResponseWriter, _ *http.Request) {
-		w.Header().Set("Content-Type", "application/json")
-		w.Header().Set("Access-Control-Allow-Origin", "*")
-
-		err := r.Walk(func(route *mux.Route, r *mux.Router, ancestors []*mux.Route) error {
-			output := make(map[string]string, 15)
-			
-			if path, err := route.GetPathTemplate(); err == nil {
-				output["route"] = path
-			}
-
-			if methods, err := route.GetMethods(); err == nil {
-				output["methods"] = strings.Join(methods, ",")
-			}
-
-			respond(&w, http.StatusOK, struct {
-					Path    string `json:"route"`
-					Methods string `json:"methods"`
-				}{
-					Path: output["route"],
-					Methods: output["methods"],
-				})
-			
-			return nil
-		})
-
-		if err != nil {
-			respond(&w, http.StatusInternalServerError, Error{
-					Message: ErrorInternal,
-				})
-
-			log.Println(err)
-			return
-		}
-	}
-}
-
-func
-usercreation() http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Content-Type", "application/json")
-		w.Header().Set("Access-Control-Allow-Origin", "*")
-		if r.Method == "OPTIONS" {
-			return
-		}
-
-		data, err := parsereq(&w, r)
-		if err != nil {
-			return
-		}
-
-		if len(data["password"]) < 8 {
-			respond(&w, http.StatusBadRequest, Error{
-					Message: ErrorShortPass,
-				})
-
-			return
-		}
-
-		password, _ := bcrypt.GenerateFromPassword([]byte(data["password"]), 12)
-
-		re := regexp.MustCompile(`^[a-zA-Z\d.\-]+@[a-zA-Z\d\-]+\.[a-zA-Z.]`).MatchString
-		if !re(data["email"]) {
-			respond(&w, http.StatusBadRequest, Error{
-					Message: ErrorEmailFormat,
-				})
-
-			return
-		}
-
-		u := User{
-			Email:    strings.ToLower(data["email"]),
-			Password: password,
-		}
-
-		var tmp User
-		db.Where("email = ?", u.Email).First(&tmp)
-		if tmp.ID != 0 {
-			respond(&w, http.StatusForbidden, Error{
-					Message: ErrorUserExist,
-				})
-			return
-		}
-
-		db.Create(&u)
-
-		respond(&w, http.StatusCreated, struct {
-				UserID uint `json:"user_id"`
-			}{
-				UserID: u.ID,
-			})
-	}
-}
-
-func
-userlogin() http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Content-Type", "application/json")
-		w.Header().Set("Access-Control-Allow-Origin", "*")
-		if r.Method == "OPTIONS" {
-			return
-		}
-
-		data, err := parsereq(&w, r)
-		if err != nil {
-			return
-		}
-
-		var u User
-
-		db.Where("email = ?", strings.ToLower(data["email"])).First(&u)
-		if u.ID == 0 {
-			respond(&w, http.StatusForbidden, Error{
-					Message: ErrorBadCredentials,
-				})
-			return
-		}
-
-		if err := bcrypt.CompareHashAndPassword(u.Password, []byte(data["password"])); err != nil {
-			respond(&w, http.StatusForbidden, Error{
-					Message: ErrorBadCredentials,
-				})
-			return
-		}
-
-		if len(u.Token) == 0 || !ValidateToken(u.Token) {
-			u.Token, u.ExpiresAt, err = CreateToken(strconv.Itoa(int(u.ID)))
-			if err != nil {
-				log.Println(err)
-				respond(&w, http.StatusInternalServerError, Error{
-						Message: ErrorInternal,
-					})
-				return
-			}
-
-			db.Save(&u)
-		}
-
-
-		respond(&w, http.StatusOK, struct {
-				TokenType   string `json:"token_type"`
-				AccessToken string `json:"access_token"`
-				ExpiresIn   int64  `json:"expires_in"`
-				UserID      uint   `json:"user_id"`
-			}{
-				TokenType:   "bearer",
-				AccessToken: u.Token,
-				ExpiresIn:   u.ExpiresAt - time.Now().Unix(),
-				UserID:      u.ID,
-			})
-	}
-}
-
-func
-userinfo() http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		token := strings.Split(r.Header.Get("Authorization"), "Bearer ")
-		id, err := strconv.Atoi(mux.Vars(r)["id"])
-		if err != nil {
-			w.WriteHeader(http.StatusBadRequest)
-			return
-		}
-
-		var ( 
-			u	User
-			udata	UserData
-		)
-
-		db.Where("token = ?", token[1]).First(&u)
-		if u.ID != uint(id) {
-			respond(&w, http.StatusForbidden, Error{
-					Message: ErrorAccess,
-				})
-			return
-		}
-
-		if r.Method == "GET" {
-			db.Where("user_id = ?", uint(id)).First(&udata)
-			if udata.UserID == 0 {
-				respond(&w, http.StatusNotFound, Error{
-						Message: ErrorNotFound,
-					})
-				return
-			}
-
-			respond(&w, http.StatusOK, struct {
-					FirstName string `json:"first_name"`
-					LastName  string `json:"last_name"`
-				}{
-					FirstName: udata.FirstName,
-					LastName:  udata.LastName,
-				})
-
-			return
-		}
-
-		data, err := parsereq(&w, r)
-		if err != nil {
-			return
-		}
-
-		re := regexp.MustCompile(`^\p{L}+$`).MatchString
-		if !re(data["first_name"]) || !re(data["last_name"]) {
-			respond(&w, http.StatusBadRequest, Error{
-					Message: ErrorNameFormat,
-				})
-
-			return
-		}
-
-		db.Where("user_id = ?", uint(id)).First(&udata)
-
-		udata.LastName = data["last_name"]
-		udata.FirstName = data["first_name"]
-
-		if udata.ID == 0 {
-			respond(&w, http.StatusCreated, struct {
-					FirstName string `json:"first_name"`
-					LastName  string `json:"last_name"`
-				}{
-					FirstName: udata.FirstName,
-					LastName:  udata.LastName,
-				})
-
-			udata.UserID = u.ID
-			db.Create(&udata)
-
-			return
-		}
-
-		w.WriteHeader(http.StatusForbidden)
-	}
-}
-
-// Util funcs
-func
-respond(w *http.ResponseWriter, status int, data interface{}) {
-	res, _ := json.MarshalIndent(data, "", "	")
-
+	(*w).Header().Set("Content-Type", "application/json")
 	(*w).WriteHeader(status)
 	(*w).Write(res)
 }
 
-func
-parsereq(w *http.ResponseWriter, r *http.Request) (map[string]string, error) {
-	var data map[string]string
-
-	body, err := ioutil.ReadAll(r.Body)
-	if err != nil {
-		respond(w, http.StatusInternalServerError, Error{
-				Message: ErrorInternal,
-			})
-		return nil, err
-	}
-
-	err = json.Unmarshal(body, &data)
-	if err != nil {
-		respond(w, http.StatusBadRequest, Error{
-				Message: ErrorBadJSON,
-			})
-		return nil, err
-	}
-
-	return data, nil
+func RespondWithError(w *http.ResponseWriter, status int, err string) {
+	RespondWithJSON(w, status, map[string]string{"error": err})
 }
 
 // Middleware
-func
-auth(next http.HandlerFunc) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Content-Type", "application/json")
-		w.Header().Set("Access-Control-Allow-Origin", "*")
-		w.Header().Set("Access-Control-Allow-Headers", "Authorization")
-		if r.Method == "OPTIONS" {
+func (a *Api) isAuth(n http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, req *http.Request) {
+		header := strings.Split(req.Header.Get("Authorization"), "Bearer ")
+		if len(header) != 2 || !a.validateToken(header[1]) {
+			RespondWithError(&w, http.StatusBadRequest, ErrorBadToken)
 			return
 		}
 
-		header := strings.Split(r.Header.Get("Authorization"), "Bearer ")
-		if len(header) != 2 || header[0] == "null" {
-			respond(&w, http.StatusBadRequest, Error{
-					Message: ErrorBadToken,
-				})
-			return
-		}
-
-		if !ValidateToken(header[1]) {
-			respond(&w, http.StatusUnauthorized, Error{
-					Message: ErrorBadToken,
-				})
-			return
-		}
-
-		next.ServeHTTP(w, r)
+		n.ServeHTTP(w, req)
 	}
 }
 
-func
-CreateToken(issuer string) (string, int64, error) {
-	expiresAt := time.Now().Add(time.Hour * 12).Unix()
+func (a *Api) generateToken(iss string) (string, int64, error) {
+	exp := time.Now().Add(time.Hour * 12).Unix()
 	claims := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.StandardClaims{
-		Issuer:    issuer,
-		ExpiresAt: expiresAt,
+		Issuer:    iss,
+		ExpiresAt: exp,
 	})
 
-	token, err := claims.SignedString([]byte(secret))
+	tok, err := claims.SignedString([]byte(a.Config.Secret))
 	if err != nil {
 		return "", 0, err
 	}
-	return token, expiresAt, nil
+
+	return tok, exp, nil
 }
 
-func
-ValidateToken(tokenString string) bool {
-	token, err := parseToken(tokenString)
+func (a *Api) validateToken(s string) bool {
+	tok, err := a.parseToken(s)
 	if err != nil {
 		return false
 	}
 
-	if _, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
+	if _, ok := tok.Claims.(jwt.MapClaims); ok && tok.Valid {
 		return true
 	}
 
 	return false
 }
 
-func
-parseToken(tokenString string) (*jwt.Token, error) {
-	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
-		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
-			return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
+func (a *Api) parseToken(s string) (*jwt.Token, error) {
+	tok, err := jwt.Parse(s, func(tok *jwt.Token) (interface{}, error) {
+		if _, ok := tok.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("Unexpected signing method")
 		}
-		return []byte(secret), nil
+
+		return []byte(a.Config.Secret), nil
 	})
 
-	return token, err
+	return tok, err
 }
 
-// Driver
-func
-main() {
+// Endpoint functions' implementations
 
-	log.Println("Initializing config structure")
-	if err := initcfg(); err != nil {
-		log.Fatal(err)
+// /
+func (a *Api) listRoutes (w http.ResponseWriter, _ *http.Request) {
+	err := a.Router.Walk(func(ro *mux.Route, r *mux.Router, anc []*mux.Route) error {
+		var (
+			path string
+			methods []string
+			err error
+		)
+
+		path, err = ro.GetPathTemplate()
+		if err != nil {
+			return err
+		}
+
+		methods, err = ro.GetMethods()
+		if err != nil {
+			return err
+		}
+			
+		RespondWithJSON(&w, http.StatusOK, map[string]string{
+			"path": path, "methods": strings.Join(methods, ",")})
+		
+		return nil
+	})
+
+	if err != nil {
+		RespondWithError(&w, http.StatusInternalServerError, ErrorInternal)
+		log.Println(err)
+		return
+	}
+}
+
+// /users
+func (a *Api) getUsers(w http.ResponseWriter, req *http.Request) {
+	
+}
+
+// /user
+func (a *Api) createUser(w http.ResponseWriter, req *http.Request) {
+	var pl struct {
+		Email		string `json:"email"`
+		Password	string `json:"password"`
 	}
 
-	log.Println("Initializing database connections pool")
-	if err := initdb(); err != nil {
-		log.Fatal(err)
+	dec := json.NewDecoder(req.Body)
+	if err := dec.Decode(&pl); err != nil {
+		RespondWithError(&w, http.StatusBadRequest, ErrorBadJSON)
+		return
+	}
+	defer req.Body.Close()
+
+	if !validateRegData(pl.Password, pl.Email) {
+		RespondWithError(&w, http.StatusBadRequest, ErrorBadJSON)
+		return
 	}
 
-	log.Println("Starting local server")
-	if err := http.ListenAndServe(port, initrouter()); err != nil {
-		log.Fatal(err)
+	pass, _ := bcrypt.GenerateFromPassword([]byte(pl.Password), 12)
+	u := User{
+		Email:    strings.ToLower(pl.Email),
+		Password: pass,
 	}
+
+	var t User
+	a.DB.Where("email = ?", u.Email).First(&t)
+	if t.ID != 0 {
+		RespondWithError(&w, http.StatusForbidden, ErrorUserExist)
+		return
+	}
+
+	a.DB.Create(&u)
+
+	RespondWithJSON(&w, http.StatusCreated, map[string]uint{"uid": u.ID}) 
+}
+
+func validateRegData(pass, email string) bool {
+	re := regexp.MustCompile(`^[a-zA-Z\d.\-]+@[a-zA-Z\d\-]+\.[a-zA-Z.]`).MatchString
+
+	if len(pass) < 8 || !re(email) {
+		return false
+	}
+
+	return true
+}
+
+// /user/auth
+func (a *Api) loginUser(w http.ResponseWriter, req *http.Request) {
+	var pl struct {
+		Email		string `json:"email"`
+		Password	string `json:"password"`
+	}
+
+	dec := json.NewDecoder(req.Body)
+	if err := dec.Decode(&pl); err != nil {
+		RespondWithError(&w, http.StatusBadRequest, ErrorBadJSON)
+		return
+	}
+	defer req.Body.Close()
+
+	var u User
+	a.DB.Where("email = ?", strings.ToLower(pl.Email)).First(&u)
+	if u.ID == 0 || !validatePassHash(u.Password, []byte(pl.Password)) {
+		RespondWithError(&w, http.StatusForbidden, ErrorBadCredentials)
+		return
+	}
+
+	if len(u.Token) == 0 || !a.validateToken(u.Token) {
+		var err error
+		u.Token, u.ExpiresAt, err = a.generateToken(strconv.Itoa(int(u.ID)))
+		if err != nil {
+			log.Println(err)
+			RespondWithError(&w, http.StatusInternalServerError, ErrorInternal)
+			return
+		}
+
+		a.DB.Save(&u)
+	}
+
+
+	RespondWithJSON(&w, http.StatusOK, struct {
+			TokenType   string `json:"token_type"`
+			AccessToken string `json:"access_token"`
+			ExpiresIn   int64  `json:"expires_in"`
+			UserID      uint   `json:"user_id"`
+		}{
+			TokenType:   "bearer",
+			AccessToken: u.Token,
+			ExpiresIn:   u.ExpiresAt - time.Now().Unix(),
+			UserID:      u.ID,
+		})
+}
+
+func validatePassHash(a []byte, b []byte) bool {
+	err := bcrypt.CompareHashAndPassword(a, b)
+	if err != nil {
+		return false
+	}
+	return true
+}
+
+// /user/id
+func (a *Api) getUser(w http.ResponseWriter, req *http.Request) {
+	var udata UserData
+	id, _ := strconv.Atoi(mux.Vars(req)["id"])
+
+	a.DB.Where("user_id = ?", uint(id)).First(&udata)
+	if udata.UserID == 0 {
+		RespondWithError(&w, http.StatusNotFound, ErrorNotFound)
+		return
+	}
+
+	RespondWithJSON(&w, http.StatusOK, map[string]string{
+			"first_name": udata.FirstName,
+			"middle_name": udata.MiddleName,
+			"last_name": udata.LastName})
+}
+
+func (a *Api) updateUser(w http.ResponseWriter, req *http.Request) {
+	var pl struct {
+		First	string `json:"first_name"`
+		Middle	string `json:"middle_name"`
+		Last	string `json:"last_name"`
+	}
+
+	dec := json.NewDecoder(req.Body)
+	if err := dec.Decode(&pl); err != nil {
+		RespondWithError(&w, http.StatusBadRequest, ErrorBadJSON)
+		return
+	}
+	defer req.Body.Close()
+
+	re := regexp.MustCompile(`^\p{L}+$`).MatchString
+	if !re(pl.First) || !re(pl.Middle) || !re(pl.Last) {
+		RespondWithError(&w, http.StatusBadRequest, ErrorNameFormat)
+		return
+	}
+
+	var udata UserData
+	id, _ := strconv.Atoi(mux.Vars(req)["id"])
+
+	a.DB.Where("user_id = ?", uint(id)).First(&udata)
+
+	if udata.ID != 0 {
+		RespondWithError(&w, http.StatusForbidden, "You can't change your name")
+	}
+
+	udata.FirstName = pl.First
+	udata.MiddleName = pl.Middle
+	udata.LastName = pl.Last
+	udata.UserID = uint(id)
+
+	a.DB.Create(&udata)
+
+	RespondWithJSON(&w, http.StatusCreated, map[string]string{
+			"first_name": udata.FirstName,
+			"middle_name": udata.MiddleName,
+			"last_name": udata.LastName})
+
+}
+
+func (a *Api) deleteUser(w http.ResponseWriter, req *http.Request) {
+
+}
+
+func (a *Api) checkPerms(n http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, req *http.Request) {
+		tok := strings.Split(req.Header.Get("Authorization"), "Bearer ")
+		id, _ := strconv.Atoi(mux.Vars(req)["id"])
+
+		var u User
+		a.DB.Where("token = ?", tok[1]).First(&u)
+		if u.ID != uint(id) {
+			RespondWithError(&w, http.StatusForbidden, ErrorAccess)
+			return
+		}
+
+		n.ServeHTTP(w, req)
+	}
+}
+
+// /courses
+func (a *Api) getCourses(w http.ResponseWriter, req *http.Request) {
+
+}
+
+// /course
+func (a *Api) getCourse(w http.ResponseWriter, req *http.Request) {
+
+}
+
+func (a *Api) createCourse(w http.ResponseWriter, req *http.Request) {
+
+}
+
+func (a *Api) updateCourse(w http.ResponseWriter, req *http.Request) {
+
+}
+
+func (a *Api) deleteCourse(w http.ResponseWriter, req *http.Request) {
+
 }
